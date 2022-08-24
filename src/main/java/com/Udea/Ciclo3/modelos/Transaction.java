a2e2011242e3e83ad13bf08b6ed3b6f228d46777
John James <ospina.john@correounivalle.edu.co>
2022-08-23 20:50:50 -0500

    Class Transaction con atributos

--- src/main/java/com/Udea/Ciclo3/modelos/Transaction.java
+++ src/main/java/com/Udea/Ciclo3/modelos/Transaction.java
@@ -1,5 +1,21 @@
 package com.Udea.Ciclo3.modelos;
 
+import javax.persistence.*;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "transaction")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+    private float amount;
+    private String concept;
+    private String updateAt;
+    private String createdAt;
+
+    private String user; // Hay que modificarlo porque debe hacer llamado a una Clase
+    private Enterprise enterprise;
+
 
 }
