815099629e4f54ae74fb11b8c47fe7844653b032
livemilton <110510932+livemilton@users.noreply.github.com>
2022-08-23 21:37:43 -0500

    Update Transaction.java

--- src/main/java/com/Udea/Ciclo3/modelos/Transaction.java
+++ src/main/java/com/Udea/Ciclo3/modelos/Transaction.java
@@ -3,12 +3,13 @@ package com.Udea.Ciclo3.modelos;
 import javax.persistence.*;
 import javax.persistence.Table;
 import java.util.Date;
-
+//Entidad para la clase transaccion
 @Entity
 @Table(name = "transaction")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    //atributos
     private long Id;
     private float amount;
     private String concept;
@@ -17,6 +18,16 @@ public class Transaction {
 
     private Employee user;
     private Enterprise enterprise;
+    //constructor
+    
+    
+    
+    
+    //getters and setters
 
-
+    
+    
+    
+    
+    
 }
