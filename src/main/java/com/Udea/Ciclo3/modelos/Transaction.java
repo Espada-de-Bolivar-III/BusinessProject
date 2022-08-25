b95b43b867a182cde025bdb77adf69b21fa24e7e
JairoFlorez96 <jaflome10@gmail.com>
2022-08-24 21:31:54 -0500

    Constructor, getter and setter

--- src/main/java/com/Udea/Ciclo3/modelos/Transaction.java
+++ src/main/java/com/Udea/Ciclo3/modelos/Transaction.java
@@ -19,15 +19,77 @@ public class Transaction {
     private Employee user;
     private Enterprise enterprise;
     //constructor
-    
-    
-    
-    
+
+    public Transaction() {
+    }
+
+    public Transaction(long id, float amount, String concept, Date updateAt, Date createdAt, Employee user, Enterprise enterprise) {
+        Id = id;
+        this.amount = amount;
+        this.concept = concept;
+        this.updateAt = updateAt;
+        this.createdAt = createdAt;
+        this.user = user;
+        this.enterprise = enterprise;
+    }
+
+
     //getters and setters
 
-    
-    
-    
-    
-    
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Employee getUser() {
+        return user;
+    }
+
+    public void setUser(Employee user) {
+        this.user = user;
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
 }
