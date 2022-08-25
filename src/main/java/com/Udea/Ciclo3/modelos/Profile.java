779b3da0cf8bb84d85c33d4ce6f2a20b93d81d23
CristianGarci42022 <cristiangarci42o22@gmail.com>
2022-08-24 23:29:12 -0500

    Creacion de Atributos, Constructor, getters ...

--- src/main/java/com/Udea/Ciclo3/modelos/Profile.java
+++ src/main/java/com/Udea/Ciclo3/modelos/Profile.java
@@ -1,10 +1,81 @@
 package com.Udea.Ciclo3.modelos;
+import javax.persistence.*;
+import javax.persistence.Table;
+import java.util.Date;
 
+@Entity
+@Table (name="Empresa")
 public class Profile {
+    //el Id es el que permite generar el identificador para el PK de la base de datos en (postgresSQL)
+    @id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //atributos
+    private String Id;
+    private String image;
+    private String phone;
+    private Employee user;
+    private Date createdAt;
+    private Date updateAt;
+    //constructor vacio
+    public Profile() {
+    }
+    //constructor
+    public Profile( String Id, String image, String phone, Employee user, Date createdAt, Date updateAt ) {
+        this.Id = Id;
+        this.image = image;
+        this.phone = phone;
+        this.user = user;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+    }
+     //getters and setters
 
+    public String getId() {
+        return Id;
+    }
 
+    public void setId(String id) {
+        this.Id = id;
+    }
 
+    public String getImage() {
+        return image;
+    }
 
+    public void setImage(String image) {
+        this.image = image;
+    }
 
+    public String getPhone() {
+        return phone;
+    }
 
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Employee getUser() {
+        return user;
+    }
+
+    public void setUser(Employee user) {
+        this.user = user;
+    }
+    
 }
